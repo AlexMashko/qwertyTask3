5cf8d13a7a950290a383344ff011b33f94ae3de4
Alex <35632159+AlexMashko@users.noreply.github.com>
2018-02-25 22:14:54 +0200

    Add files via upload

--- public/firebaseauthscript.js
+++ public/firebaseauthscript.js
@@ -6,7 +6,7 @@
         document.getElementById('register').addEventListener('click', handleSignUp, false);
 
         function handleSignUp() {
-            //console.log('Up');
+            
             var name = document.getElementById('register_name').value;
             var email = document.getElementById('register_login').value;
             var password = document.getElementById('register_password').value;
@@ -34,23 +34,19 @@
                 var errorCode = error.code;
                 var errorMessage = error.message;
                 if (errorCode == 'auth/weak-password') {
-
                     alert('The password is too weak.');
-
                 } else {
                     messageAlertRegister.className=('alert ');
                     messageAlertRegister.innerHTML = " ";
                     alert(errorMessage);
                 }
                 console.log(error);
-                messageAlertRegister.className=('alert alert-success');
-                messageAlertRegister.innerHTML = "<strong>Успешно!</strong> создана Ваша учетная запись.";
-
+                //messageAlertRegister.className=('alert alert-success');
+                //messageAlertRegister.innerHTML = "<strong></strong>  Ваша учетная запись существует.";
+              
             });
-            sendEmailVerification();
             // [Заканчиваем регистрацию]
-            
-           
+
         }
 
 
@@ -63,7 +59,7 @@ document.getElementById('auth').addEventListener('click', toggleSignIn, false);
          * Функционал кнопки sign in/sign out
          */
         function toggleSignIn() {
-            
+
             // Если пользователь залогинен
             if (firebase.auth().currentUser) {
                 // [Начинаем разлогиниваться]
@@ -90,7 +86,7 @@ document.getElementById('auth').addEventListener('click', toggleSignIn, false);
                 // [Начинаем логин через email]
                 firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
                     // В случае ошибок
-                    //var messageAlertAuth=document.getElementById('auth_form_message');
+                    
                     var errorCode = error.code;
                     var errorMessage = error.message;
                     if (errorCode === 'auth/wrong-password') {
@@ -111,38 +107,21 @@ document.getElementById('auth').addEventListener('click', toggleSignIn, false);
 
 
 
-        function sendEmailVerification() {
-            firebase.auth().currentUser.sendEmailVerification().then(function() {
-                //alert('Проверьте вашу почту и подтвердите ваш Email!');
-            });
-        }
-
-
         function initApp() {
             
-            //console.log(firebase.database());
+            
             // Слушаем статус аутентификации
             firebase.auth().onAuthStateChanged(function(user) {
                 // [Выключаем другую кнопку]
- 
+                
                 var messageAlertRegister=document.getElementById('register_form_message');
                 var messageAlertAuth=document.getElementById('auth_form_message');
                 
-
                 if (user) {
                     // Если пользователь залогинен
-                    var email = user.email;
-                    var emailVerified = user.emailVerified; console.log('emalverifyed: '+emailVerified);
-                    if(!emailVerified){
-                            console.log('verify email send');
-                            messageAlertAuth.className=('alert alert-danger');
-                            messageAlertAuth.innerHTML = "Email :<strong>"+email+" не подтвержден!</strong> Проверьте почту и подтвердите email. ";
-                            //sendEmailVerification();
-                            return false;
-                    };
                     //console.log('user=true');
                     var name =document.getElementById('register_name').value;
-                    console.log('name:'+name);
+                    //console.log('name:'+name);
                     if (name) {//если пользователь авторизовался и указал имя то добавляем имя.
                         firebase.auth().currentUser.updateProfile({
                         displayName: name
@@ -151,8 +130,8 @@ document.getElementById('auth').addEventListener('click', toggleSignIn, false);
                         });
                     }
                     var displayName = user.displayName; if(!displayName){displayName=" ";}
-                    //var email = user.email;
-                    //var emailVerified = user.emailVerified;console.log(emailVerified);
+                    var email = user.email;
+                    var emailVerified = user.emailVerified;//console.log(emailVerified);
                     var photoURL = user.photoURL;
                     var isAnonymous = user.isAnonymous;
                     var uid = user.uid;
@@ -164,16 +143,12 @@ document.getElementById('auth').addEventListener('click', toggleSignIn, false);
                     messageAlertRegister.className='alert alert-success';
                     messageAlertRegister.innerHTML = "Здарвствуйте <strong>"+displayName+"</strong> ,Вы выполнили вход используя почту: "+"<strong>"+email+"</strong>";
                     messageAlertAuth.className='alert alert-success';
-                    messageAlertAuth.innerHTML = "Здарвствуйте <strong>"+displayName+"</strong> ,Вы выполнили вход используя почту: "+"<strong>"+email+"</strong>";
-                    
-
-
-                    
+                    messageAlertAuth.innerHTML = "Здарвствуйте <strong>"+displayName+"</strong> ,Вы уже выполнили вход используя почту: "+"<strong>"+email+"</strong>";           
                     // [Устанавливаем текст на кнопках]                    
                     document.getElementById('auth').textContent = 'Выйти';
                     //var details=document.getElementById('account-details').textContent = JSON.stringify(user, null, '  ');
                     // if (!emailVerified) {
-                    //     document.getElementById('quickstart-verify-email').disabled = false;
+                    //      // sendEmailVerification();
                     // }
             
                     console.log('mail: '+email+ ' name:'+displayName);
@@ -187,18 +162,22 @@ document.getElementById('auth').addEventListener('click', toggleSignIn, false);
                     messageAlertAuth.innerHTML = "";
                     document.getElementById('auth').textContent = 'Войти';
                     document.getElementById('sidebar-addition-functions').style.display='none';
-                    //document.getElementById('quickstart-account-details').textContent = 'null';
+
                 }
                 // [Выключаем другую кнопку]
                 document.getElementById('auth').disabled = false;
             });
 
-            //document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
-            //document.getElementById('quickstart-sign-up').addEventListener('click', handleSignUp, false);
-            //document.getElementById('quickstart-verify-email').addEventListener('click', sendEmailVerification, false);
-            //document.getElementById('quickstart-password-reset').addEventListener('click', sendPasswordReset, false);
+
         }
 
         window.onload = function() {
             initApp();
         };
+
+
+        function sendEmailVerification() {//отправляем текущему пользователю email с подтвержением
+            firebase.auth().currentUser.sendEmailVerification().then(function() {
+                alert('Проверьте вашу почту и подтвердите ваш Email!');
+            });
+        }
